cee92cc6930985149eb3430322d4e40d99620e3a
greenvald <greenvald@gmail>
2020-07-26 20:19:21 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reactive-forms';
+  constructor(private pb:FormBuilder){}
+  registrationForm = this.pb.group({
+    username:[''],
+    password:[''],
+    confirmPassword:[''],
+    adrress : this.pb.group({
+      city:[''],
+      state:[''],
+      postalCode:['']
+
+    }
+      
+    )
+
+
+  })
+
+
+
+//   registrationForm = new FormGroup({
+//     username: new FormControl(''),
+//     password: new FormControl(''),
+//     confirmPassword: new FormControl(''),
+//     adrress : new FormGroup({
+//     city: new FormControl(''),
+//     state: new FormControl(''),
+//     postalCode: new FormControl('')
+//   })
+// })
+loadApiData(){
+  return this.registrationForm.patchValue({
+    username:'aron',
+    password:'8888',
+    confirmPassword:'9999'
+
+  })
+}
 }
